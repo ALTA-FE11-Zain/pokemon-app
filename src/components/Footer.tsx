3068e388d9729c6e6e25011d7b082abcaccfe1a3
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-15 02:18:36 +0700

    style: changed bottom menu icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,30 +4,30 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="navbar fixed bottom-0 items-center px-14 sm:px-28 md:px-40 lg:px-48 xl:px-60 bg-gray-300">
-      <a className="navbar-start">
+      <div className="navbar-start">
         <Link
-          to=""
+          to="/"
           className="text-3xl duration-300 hover:cursor-pointer active:scale-75"
         >
           <TbSettings />
         </Link>
-      </a>
-      <a className="navbar-center">
+      </div>
+      <div className="navbar-center">
         <Link
           to="/"
           className="text-3xl duration-300 hover:cursor-pointer active:scale-75"
         >
           <TbHome />
         </Link>
-      </a>
-      <a className="navbar-end">
+      </div>
+      <div className="navbar-end">
         <Link
           to="/mypokemon"
           className="text-3xl duration-300 hover:cursor-pointer active:scale-75"
         >
           <TbPokeball />
         </Link>
-      </a>
+      </div>
     </footer>
   );
 };
